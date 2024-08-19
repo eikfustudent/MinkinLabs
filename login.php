7be59ec52c130db9b2375bfb9a848392ea21d5d2
eikfustudent <155021083+eikfustudent@users.noreply.github.com>
2024-08-19 18:36:48 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,23 @@
+<?php
+require 'db.php';
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt->execute([$username]);
+    $user = $stmt->fetch();
+
+    if ($user && password_verify($password, $user['password'])) {
+        session_start();
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+        header('Location: ../index.html');
+        exit();
+    } else {
+        header('Location: ../login.html');
+        exit();
+    }
+}
+?>
