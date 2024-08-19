04f8bb6196bb5fce2dd0a6ffb797e615dbb6f1a1
eikfustudent <155021083+eikfustudent@users.noreply.github.com>
2024-08-19 18:31:19 +0300

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -1,1 +1,18 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
+    $to = 'admin@example.com';
+    $subject = 'Обратная связь';
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    mail($to, $subject, $message, $headers);
+
+    header('Location: ../feedback.html');
+    exit();
+}
+?>
