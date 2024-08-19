2f0851518317f78d88f9cb85f56e73978717e215
eikfustudent <155021083+eikfustudent@users.noreply.github.com>
2024-08-19 18:30:52 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,1 +1,19 @@
+<?php
+$host = 'localhost';
+$db   = 'your_database';
+$user = 'your_username';
+$pass = 'your_password';
+$charset = 'utf8mb4';
 
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
