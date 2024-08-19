923fa6f6be24a489b3ae5991a60810a6c46ddc5c
eikfustudent <155021083+eikfustudent@users.noreply.github.com>
2024-08-19 18:31:35 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,14 @@
+<?php
+require 'db.php';
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $user_id = 1; // Замените на реальный идентификатор пользователя
+    $score = $_POST['score'];
+
+    $stmt = $pdo->prepare("INSERT INTO test_results (user_id, score) VALUES (?, ?)");
+    $stmt->execute([$user_id, $score]);
+
+    header('Location: ../test.html');
+    exit();
+}
+?>
